90956418b1a3cb8a970aa96c85a39df4b3c5f867
kaustubh077 <87428675+kaustubh077@users.noreply.github.com>
2021-09-07 23:09:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+const Slackbot = require("slackbots");
+const axios = require("axios");
+const dotenv = require("dotenv");
+const sharp = require('sharp');
+dotenv.config()
+
+const bot = new Slackbot({
+    token: `${process.env.BOT_TOKEN}`,
+    name: 'demobotapp'
+})
+
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':robot_face:'
+    }
+
+    bot.postMessageToChannel(
+        'test',
+        'Test message h, aajaye to badhiya',
+        params
+    );
+})
+
+bot.on('error', (err) => {
+    console.log(err);
+})
+
+bot.on('message', (data) => {
+    console.log(data);
+    if(data.type !== 'message') {
+        return;
+    }
+    if(data.files){
+        console.log("data files detected----------------------------------------------------------------------------------",data.user)
+        data.files.forEach(file => {
+            if(file.filetype == "png"){
+                console.log("png files detected----------------------------------------------------------------------------------")
+                console.log("conversion started----------------------------------------------------------------------------------")
+                sharp(file.url_private).webp().toBuffer().then(img => {
+                    console.log("conversion finished----------------------------------------------------------------------------------");
+                    bot.postMessageToChannel(
+                    'test',
+                    `:zap: this works`,
+                    img,
+                );
+                })
+                .catch(err => console.log(err));
+            }
+        })
+    }
+    handleMessage(data.text);
+})
+
+// async function convert(){
+//     const outputWebp = sharp(imgurl).webp().toBuffer();
+//     return outputWebp;
+// }
+
+function handleMessage(message) {
+    if(message.includes(' inspire me')) {
+        // inspireMe()
+        bot.postMessageToChannel(
+            'test',
+            `:zap: this works`,
+        );
+    } else if(message.includes(' random joke')) {
+        // randomJoke()
+        console.log("joked")
+    } else if(message.includes(' help')) {
+        // runHelp()
+        console.log("helped")
+    }
+}
