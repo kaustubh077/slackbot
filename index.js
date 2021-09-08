fa1546fe319e89e5b169b385aa20cd95f0a05e3a
kaustubh077 <87428675+kaustubh077@users.noreply.github.com>
2021-09-08 22:00:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,35 @@
 const Slackbot = require("slackbots");
 const axios = require("axios");
+var express = require('express')
+var request = require('request')
+var bodyParser = require('body-parser')
+var app = express()
+// var deeplink = require('node-deeplink');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 const dotenv = require("dotenv");
-const sharp = require('sharp');
+// const sharp = require('sharp');
 dotenv.config()
+var app = express();
+
+// Lets start our server
+app.listen(process.env.PORT, function () {
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Example app listening on port " + process.env.PORT);
+});
+
+// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
+app.get('/', function(req, res) {
+    res.send('Ngrok is working! Path Hit: ' + req.url);
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//BOT STARTING
 
 const bot = new Slackbot({
     token: `${process.env.BOT_TOKEN}`,
     name: 'demobotapp'
 })
-
-bot.on('start', () => {
-    const params = {
-        icon_emoji: ':robot_face:'
-    }
-
-    bot.postMessageToChannel(
-        'test',
-        'Test message onstartup',
-        params
-    );
-})
-
-bot.on('error', (err) => {
-    console.log(err);
-})
-
-bot.on('message', (data) => {
-    console.log(data);
-    if(data.type !== 'message') {
-        return;
-    }
-    if(data.files){
-        console.log("data files detected----------",data.user)
-        data.files.forEach(file => {
-            if(file.filetype == "png"){
-                console.log("png files detected---------------------------")
-                console.log("conversion started-------------------------")
-                sharp(file.url_private).webp().toBuffer().then(img => {
-                    console.log("conversion finished---------------------");
-                    bot.postMessageToChannel(
-                    'test',
-                    `:zap: this works`,
-                    img,
-                );
-                })
-                .catch(err => console.log(err));
-            }
-        })
-    }
-    handleMessage(data.text);
-})
-//  test code
+//trial message " inspire me"
 function handleMessage(message) {
     if(message.includes(' inspire me')) {
         // inspireMe()
@@ -58,9 +37,143 @@ function handleMessage(message) {
             'test',
             `:zap: this works`,
         );
-    } else if(message.includes(' random joke')) {
-        console.log("joked")
-    } else if(message.includes(' help')) {
-        console.log("helped")
-    }
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//MENTIOINS HANDLING, CONVERTING PNG TO WEBP
+
+// if the bot is mentioned with a png image
+bot.on('message', (data) => {
+	// console.log(data);
+	if(data.type !== 'message') {
+		return;
+	}
+	if(data.files){
+		console.log("data files detected-",data.user)
+		data.files.forEach(file => {
+			if(file.filetype == "png"){
+				const imgurl = file.url_private_download;
+				console.log("png files detected-",imgurl)
+				// the returning message, ideally contains the converted image
+				var message = {
+					"blocks":[
+						{
+							"type": "image",
+							"title": {
+								"type": "plain_text",
+								"text": "webpImage",
+								"emoji": true
+							},
+							// "image_url": "https://freepngimg.com/thumb/light/7-2-light-png-clipart.png",
+							"image_url": imgurl,// this is currently not working, if we use the above link instead of this, everything works
+							// use sharp module for conversion once the image is successfully obtained.
+							"alt_text": "image1"
+						},
+					]
+				}
+				sendMessageToSlackResponseURL(process.env.RESPONSE_URL,message);
+			}
+		})
+	}
+	handleMessage(data.text);
+})
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//SLASH COMMANDS HANDLING FOR DEEPLINKS
+
+
+// Route the endpoint that our slash command will point to and send back a simple response
+
+app.post('/command', urlencodedParser, (req, res) =>{
+	res.status(200).end() // best practice to respond with empty 200 status code
+	var reqBody = req.body
+	console.log(req);
+	var responseURL = reqBody.response_url
+	if (reqBody.token != process.env.VERIFICATION_TOKEN){
+	   	res.status(403).end("Access forbidden")
+  	}else{
+		// The message is test message from slack website just for testing.
+	 	var message = {
+			"blocks": [
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "Hey there 👋 I'm TaskBot. I'm here to help you create and manage tasks in Slack.\nThere are two ways to quickly create tasks:"
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*1️⃣ Use the `/task` command*. Type `/task` followed by a short description of your tasks and I'll ask for a due date (if applicable). Try it out by using the `/task` command in this channel."
+					}
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*2️⃣ Use the _Create a Task_ action.* If you want to create a task from a message, select `Create a Task` in a message's context menu. Try it out by selecting the _Create a Task_ action for this message (shown below)."
+					}
+				},
+				{
+					"type": "image",
+					"title": {
+						"type": "plain_text",
+						"text": "image1",
+						"emoji": true
+					},
+					"image_url": "https://api.slack.com/img/blocks/bkb_template_images/onboardingComplex.jpg",
+					"alt_text": "image1"
+				},
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "➕ To start tracking your team's tasks, *add me to a channel* and I'll introduce myself. I'm usually added to a team or project channel. Type `/invite @TaskBot` from the channel or pick a channel on the right."
+					},
+					"accessory": {
+						"type": "conversations_select",
+						"placeholder": {
+							"type": "plain_text",
+							"text": "Select a channel...",
+							"emoji": true
+						}
+					}
+				},
+			]
+		}
+  		sendMessageToSlackResponseURL(responseURL, message)
+  	}
+})
+
+// this function handles button clicks, not necessary rightnow
+app.post('/actions', urlencodedParser, (req, res) =>{
+	res.status(200).end() // best practice to respond with 200 status
+	var actionJSONPayload = JSON.parse(req.body.payload) // parse URL-encoded payload JSON string
+	var message = {
+	    "text": actionJSONPayload.user.name+" clicked: "+actionJSONPayload.actions[0].name,
+	    "replace_original": false
+	}
+	sendMessageToSlackResponseURL(actionJSONPayload.response_url, message)
+})
+
+
+function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+	var postOptions = {
+		uri: responseURL,
+		method: 'POST',
+		headers: {
+			'Content-type': 'application/json',
+			'Authorization': `Bearer ${process.env.BOT_TOKEN}`,
+		},
+		json: JSONmessage
+  	}
+  	request(postOptions, (error, response, body) => {
+  		if (error){
+  			console.log(error);
+  		}
+  	})
 }
